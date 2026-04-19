780c95e4de105929385f277c6f50d035d3cdb0ba
midi2154 <midi2154@gmail.com>
2026-04-19 18:29:57 -0400

    Update main.go

--- main.go
+++ main.go
@@ -1,1 +1,38 @@
+package transformations
 
+import (
+	"regexp"
+)
+
+
+func Quote(s string)string{
+	re := regexp.MustCompile(`'\s+(.*?)\s+'`)
+	s = re.ReplaceAllString(s, "'$1'")
+
+	re2 := regexp.MustCompile(`"\s+(.*?)\s+"`)
+	s = re2.ReplaceAllString(s,`"1$"`)
+
+
+	re3 := regexp.MustCompile((`(\w+)(\s+)`))
+	s = re3.ReplaceAllString(s, `$1 `)
+
+
+	return s
+}
+
+
+package transformations
+
+import (
+	"regexp"
+)
+
+func Punct(word string)string{
+	Checkpunct := regexp.MustCompile(`\s+([.,:;?!])`)
+	word = Checkpunct.ReplaceAllString(word, "$1")
+
+	checkpunct2 := regexp.MustCompile(`([.,:;?!])(\s*)(\w)`)
+	word = checkpunct2.ReplaceAllString(word, "$1 $3")
+
+	return word
+}
